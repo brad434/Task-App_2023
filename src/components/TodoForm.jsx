e13be5f27dae66bb91d5258c9a65371d1b94886f
Bradley Chery <Bradley434@hotmail.com>
2023-11-08 02:52:34 -0500

    updated todoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
   const [value, setValue] = useState("")
 
   const handleSubmit = e => {
     e.preventDefault();
 
-    console.log(value);
+    addTodo(value);
+    // this will clear the input after its been submitted
+    setValue("")
   }
 
   return (
     <form className="TodoForm" onSubmit={handleSubmit}>
-      <input type="text" className="todo-input" placeholder="What is the task for today?" onChange={(e) => setValue(e.target.value)} />
+      <input type="text" className="todo-input" placeholder="What is the task for today?" value={value} onChange={(e) => setValue(e.target.value)} />
       <button type="submit" className="todo-btn">Add Task</button>
     </form>
   );
--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm.jsx";
 import { v4 as uuidv4 } from 'uuid';
-//initialize it
+//initialize it. uuidv4 is a way to give generated digits with a number that is different from anyone else
 uuidv4();
 
 
@@ -15,7 +15,7 @@ const TodoWrapper = () => {
 
 
   return <div className="TodoWrapper">
-    <TodoForm />
+    <TodoForm addTodo={addTodo} />
   </div>;
 };
 
