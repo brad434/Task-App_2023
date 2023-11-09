9396e6ac48c2943f3589fb54ae2d41220561a747
Bradley Chery <Bradley434@hotmail.com>
2023-11-09 02:30:49 -0500

    updated the todo data

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm.jsx";
 import { v4 as uuidv4 } from 'uuid';
+import Todo from "./Todo.jsx";
 //initialize it. uuidv4 is a way to give generated digits with a number that is different from anyone else
 uuidv4();
 
@@ -10,12 +11,17 @@ const TodoWrapper = () => {
 
   const addTodo = todo => {
     setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }])
+
     console.log(todos)
   }
 
 
   return <div className="TodoWrapper">
+    <h1>Task App 2023</h1>
     <TodoForm addTodo={addTodo} />
+    {todos.map((todo, index) => (
+      <Todo key={index} task={todo} />
+    ))}
   </div>;
 };
 
